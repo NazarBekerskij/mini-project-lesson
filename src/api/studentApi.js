191aae9e5904586e3b6092849b1405e7bb11c82b
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-05-20 14:11:24 +0300

    save creatposts

--- src/api/studentApi.js
+++ src/api/studentApi.js
@@ -2,19 +2,28 @@ const BASE_URL = 'http://localhost:3000/posts';
 
 
 
-async function getPosts(){
-    try{
-        const res = await fetch(`${BASE_URL}`)
-        const data = await res.json()
-        return data
-    } catch{
-
-    }
+async function getPosts() {
+    const res = await fetch(BASE_URL);
+    return await res.json();
 }
 
 
 
 
+async function createPost(studentData) {
+    const options = {   
+            method: "POST",
+            body: JSON.stringify(studentData),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8",
+            },
+        };
+
+        const result = await fetch(BASE_URL, options)
+        return result
+}
 
 
+
+export {getPosts}
 
