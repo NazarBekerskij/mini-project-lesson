8a2386e37778ca3bc3795dff898f86e1282d3d63
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-05-22 21:27:33 +0300

    save form adventlisner

--- src/app.js
+++ src/app.js
@@ -4,3 +4,28 @@ getPosts("cats").then(res => console.log(res));
 
 
 
+const form = document.querySelector(".search-form")
+
+
+
+
+
+
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+
+
+    const input = event.target.query.value
+    const res = await getPosts(input)
+    console.log(res);
+    
+})
+
+
+
+
+
+
+
