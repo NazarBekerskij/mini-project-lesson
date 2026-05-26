f48ba972eb868fdc5d0a17f26e4fbb53a0ec0ea2
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-05-26 19:06:58 +0300

    save api

--- src/app.js
+++ src/app.js
@@ -53,8 +53,55 @@
 
 
 
-const url = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=5&apikey=EVDOv2DA0yM2nfxqUJW4oSne0yWf70dD';
+// const url = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=5&apikey=EVDOv2DA0yM2nfxqUJW4oSne0yWf70dD';
 
-fetch(url)
-  .then(res => res.json())
-  .then(data => console.log(data._embedded.events));
+// fetch(url)
+//   .then(res => res.json())
+//   .then(data => console.log(data._embedded.events));
+
+
+
+
+
+
+
+
+
+
+
+
+const API_KEY = "EVDOv2DA0yM2nfxqUJW4oSne0yWf70dD";
+
+const eventsList = document.querySelector(".events__list");
+
+function getEvents() {
+  return fetch(
+    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${API_KEY}&size=20&countryCode=US`
+  ).then(response => response.json());
+}
+
+function createEventsMarkup(array) {
+  return array.map(event => {
+    const image = event.images[0].url;
+    const title = event.name;
+    const date = event.dates.start.localDate;
+    const place = event._embedded.venues[0].name;
+
+    return `
+      <li class="events__item">
+        <article class="event-card">
+          <img class="event-card__image" src="${image}" alt="${title}" />
+          <h2 class="event-card__title">${title}</h2>
+          <p class="event-card__date">${date}</p>
+          <p class="event-card__place">${place}</p>
+        </article>
+      </li>
+    `;
+  }).join("");
+}
+
+getEvents().then(data => {
+  const events = data._embedded.events;
+
+  eventsList.innerHTML = createEventsMarkup(events);
+});
