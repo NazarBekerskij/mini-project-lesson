4b152a710064c42b4a37bf966b408b42574955b4
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-05-26 18:58:30 +0300

    save api

--- src/api/studentApi.js
+++ src/api/studentApi.js
@@ -1,14 +1,14 @@
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '55396511-9fa47eb753a2484966d5aafba';
-let per_page = 12;
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '55396511-9fa47eb753a2484966d5aafba';
+// let per_page = 12;
 
-async function getPosts(query, page = 1) {
-  try {
-    const res = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&page=${page}&per_page=12&image_type=photo&orientation=horizontal`);
-    return await res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function getPosts(query, page = 1) {
+//   try {
+//     const res = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&page=${page}&per_page=12&image_type=photo&orientation=horizontal`);
+//     return await res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-export { getPosts };
+// export { getPosts };
--- src/app.js
+++ src/app.js
@@ -1,49 +1,60 @@
-import { getPosts } from "./api/studentApi";
+// import { getPosts } from "./api/studentApi";
 
-// getPosts("cats").then(res => console.log(res));
+// // getPosts("cats").then(res => console.log(res));
 
-const form = document.querySelector(".search-form")
-const gallery = document.querySelector(".gallery")
-const loadMore = document.querySelector(".load-more-btn")
+// const form = document.querySelector(".search-form")
+// const gallery = document.querySelector(".gallery")
+// const loadMore = document.querySelector(".load-more-btn")
 
-let page = 1;
-let currentQuery = "";
+// let page = 1;
+// let currentQuery = "";
 
-function createItemsMarkup(array) {
-  return array.map(({ webformatURL, likes, views, comments, downloads }) => {
-    return `
-      <li>
-        <div class="photo-card">
-          <img src="${webformatURL}" alt="" />
-          <div class="stats">
-            <p class="stats-item"><i class="material-icons">thumb_up</i> ${likes}</p>
-            <p class="stats-item"><i class="material-icons">visibility</i> ${views}</p>
-            <p class="stats-item"><i class="material-icons">comment</i> ${comments}</p>
-            <p class="stats-item"><i class="material-icons">cloud_download</i> ${downloads}</p>
-          </div>
-        </div>
-      </li>
-    `;
-  }).join("");
-}
+// function createItemsMarkup(array) {
+//   return array.map(({ webformatURL, likes, views, comments, downloads }) => {
+//     return `
+//       <li>
+//         <div class="photo-card">
+//           <img src="${webformatURL}" alt="" />
+//           <div class="stats">
+//             <p class="stats-item"><i class="material-icons">thumb_up</i> ${likes}</p>
+//             <p class="stats-item"><i class="material-icons">visibility</i> ${views}</p>
+//             <p class="stats-item"><i class="material-icons">comment</i> ${comments}</p>
+//             <p class="stats-item"><i class="material-icons">cloud_download</i> ${downloads}</p>
+//           </div>
+//         </div>
+//       </li>
+//     `;
+//   }).join("");
+// }
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+// form.addEventListener("submit", async (event) => {
+//   event.preventDefault();
 
-  const input = event.target.query.value;
-  const res = await getPosts(input);
+//   const input = event.target.query.value;
+//   const res = await getPosts(input);
 
-  page = 1;
-  currentQuery = input;
-  loadMore.classList.remove("is-hidden");
-  gallery.innerHTML = createItemsMarkup(res.hits);
-});
+//   page = 1;
+//   currentQuery = input;
+//   loadMore.classList.remove("is-hidden");
+//   gallery.innerHTML = createItemsMarkup(res.hits);
+// });
 
 
-loadMore.addEventListener("click", async () => {
-  page += 1;
-  console.log(page);
+// loadMore.addEventListener("click", async () => {
+//   page += 1;
+//   console.log(page);
   
-  const res = await getPosts(currentQuery, page);
-  gallery.innerHTML += createItemsMarkup(res.hits);
-});
+//   const res = await getPosts(currentQuery, page);
+//   gallery.innerHTML += createItemsMarkup(res.hits);
+// });
+
+
+
+
+
+
+const url = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&size=5&apikey=EVDOv2DA0yM2nfxqUJW4oSne0yWf70dD';
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data._embedded.events));
